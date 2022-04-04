7eab908883ba543bfd79d04c73ad0c646a5089db
scoyoc <scoyoc@gmail.com>
2022-04-04 13:01:09 -0600

    Housekeeping

---
+++ R/utils.R
@@ -0,0 +1,7 @@
+utils::globalVariables(c("Check", "Date", "DateTime", "DateTime_new", "Details",
+                         "Element", "FileName", "Flag", "Hour", "Max", "MaxTime",
+                         "MaxTips_min", "Mean", "Min", "MinTime", "PRCP_mm",
+                         "PlotID", "Product", "RH", "RID", "Temp", "TEMP",
+                         "Time", "Tips", "Units", "Value", "Var",
+                         "max.tips.min", "mm.hr", "n", "skip", "tips.hr",
+                         "tips.min"))
